de4fea892890dac87a7e9a5a5afe85343d3832ed
Alison <alisonkim@Tests-MacBook-Air.local>
2016-06-24 22:13:34 -0400

    Camera and Movement Setup

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -1,15 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AppleTree : MonoBehaviour {
+public class AppleTree : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
+	public GameObject applePrefab;	// prefab for instantiating apples
+
+	public float speed = 1f;	// speed at which the AppleTree moves in meters/second
+	public float leftAndRightEdge = 10f;	// distance where AppleTree turns around
+	public float chanceToChangeDirections = 0.1f;	// chance that the AppleTree will change directions
+	public float secondsBetweenAppleDrops = 1f;	// rate at which apples will be instantiated
+
+	void Start ()
+	{
+		// dropping apples every second
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update ()
+	{
+		//basic movement
+		Vector3 pos = transform.position;	// defines Vector3 pos to be the current position of the AppleTree
+		pos.x += speed + Time.deltaTime;	// x of pos is increased by the speed * Time.deltaTime, measure of the # of seconds since the last frame
+		transform.position = pos;	// moves AppleTree to a new position
+		//changing direction
+		if (pos.x < -leftAndRightEdge) {
+			speed = Mathf.Abs (speed);	// move right
+		} else if (pos.x > leftAndRightEdge) {
+			speed = -Mathf.Abs (speed);	// move left
+		}
+	}
+
+	void FixedUpdate()
+	{
+		// changing direction randomly
+		if (Random.value < chanceToChangeDirections) {
+			speed *= -1;	// change direction
+		}
 	}
 }
+
+
+
+
+
